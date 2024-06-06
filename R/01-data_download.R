d14d6a0ad07ed888480957cd6de640eba302936d
Till Häussner <thaeussner@ethz.ch>
2024-06-06 19:25:48 +0000

    added dictionary

--- R/01-data_download.R
+++ R/01-data_download.R
@@ -3,7 +3,7 @@ library(googlesheets4)
 
 # import data from Google Sheets
 raw_data_private <- read_sheet("https://docs.google.com/spreadsheets/d/1xdFW_hciiYMtzxY6r0nCTOOkwYAYz7synwAnyO0wFIw/edit#gid=350931651")
-
+dictionary <- read_sheet("https://docs.google.com/spreadsheets/d/1sbEnCcIQwRlpGeICqkOFx7ZyH31O9SB3WR1n-TN7UYY/edit#gid=0")
 
 # Remove columns with sensitive, non-anonymous data
 raw_data <- raw_data_private |> 
@@ -13,3 +13,6 @@ raw_data <- raw_data_private |>
 # write raw data to csv and rds
 write_rds(raw_data, "data/raw/raw_data.rds")
 write_csv(raw_data, "data/raw/raw_data.csv")
+
+# write the dictionary to csv
+write_csv(dictionary, "data/processed/dictionary.csv")
